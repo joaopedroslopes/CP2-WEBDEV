601717cc012a26a19b558f4d9ec35985f374a170
João Pedro de Souza Lopes <jp01.bao@gmail.com>
2024-04-22 13:09:10 -0300

    finalizando-index

---
+++ script/idade.js
@@ -0,0 +1,13 @@
+function validarIdade(){
+
+    var idade = document.getElementById("input-idade").value;
+
+    if(idade >= 18){
+        alert("Você é maior de idade, pode acessar o site!");
+        window.location.href = "./home-page.html";
+    }else{
+        alert("Você é menor de idade, não pode acessar o site!");
+        window.location.href = "https://www.cnnbrasil.com.br/tudo-sobre/criancas/";
+    }
+
+}
