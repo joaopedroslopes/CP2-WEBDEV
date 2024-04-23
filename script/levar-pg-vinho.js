97d1c7e8c96e0c48e0a1d51fda57e01bcf3f8d39
João Pedro de Souza Lopes <jp01.bao@gmail.com>
2024-04-22 22:34:06 -0300

    link da home-page para as vine-pages

---
+++ script/levar-pg-vinho.js
@@ -0,0 +1,18 @@
+function vinho1(){
+
+    window.location.href = "./vinhos/vinho-1.html";
+
+}
+
+function vinho2(){
+
+    window.location.href = "./vinhos/vinho-2.html";
+
+}
+
+function vinho3(){
+
+    window.location.href = "./vinhos/vinho-3.html";
+
+}
+
